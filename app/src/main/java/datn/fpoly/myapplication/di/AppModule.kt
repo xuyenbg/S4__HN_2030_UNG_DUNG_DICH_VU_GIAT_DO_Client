2e7f1969ff872d4545bcf4faeefe1e1945e1e62f
Xuyen <xuyen97bgcity1@gmail.com>
2023-09-26 11:06:25 +0700

    xuyen test commit

--- app/src/main/java/datn/fpoly/myapplication/di/AppModule.kt
+++ app/src/main/java/datn/fpoly/myapplication/di/AppModule.kt
@@ -1,8 +1,16 @@
 package datn.fpoly.myapplication.di
 
 import dagger.Module
+import dagger.Provides
+import datn.fpoly.myapplication.data.network.RemoteDataSource
+import datn.fpoly.myapplication.utils.LocalHelper
 
 @Module
 object AppModule {
-
+    @Provides
+    fun providerLocaleHelper(): LocalHelper = LocalHelper()
+    @Provides
+    fun provideRemoteDataSource(): RemoteDataSource {
+        return RemoteDataSource()
+    }
 }
