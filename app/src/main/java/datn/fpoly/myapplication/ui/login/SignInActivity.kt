a20ac59cd94981729ac72f1df88011d81cc2aaa3
Xuyen <xuyen97bgcity1@gmail.com>
2023-09-26 18:27:44 +0700

    xuyen fix code

--- app/src/main/java/datn/fpoly/myapplication/ui/login/SignInActivity.kt
+++ app/src/main/java/datn/fpoly/myapplication/ui/login/SignInActivity.kt
@@ -8,7 +8,6 @@ import com.airbnb.mvrx.viewModel
 import datn.fpoly.myapplication.AppApplication
 import datn.fpoly.myapplication.core.BaseActivity
 import datn.fpoly.myapplication.databinding.ActivitySignInBinding
-import datn.fpoly.myapplication.utils.snackbar
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import timber.log.Timber
@@ -23,7 +22,6 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>(), LoginViewModel.Fac
         (applicationContext as AppApplication).appComponent.inject(this);
         super.onCreate(savedInstanceState)
         setContentView(views.root)
-        initUiAndData();
     }
 
     override fun initUiAndData() {
@@ -45,29 +43,29 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>(), LoginViewModel.Fac
     }
 
     private fun updateWithState(state: LoginViewState) {
-      when{
-          state.stateLogin is Success ->{
-              runBlocking {
-                  Timber.tag("Log In: ").d("LogIn...")
-                  Log.d("Log In", "LogIn...")
-                  launch {
-                     state.stateLogin.invoke()?.let{
-                         listUser ->
-                         if (listUser != null && listUser.isNotEmpty()) {
-                             Log.d("ListData", listUser.toString())
-                             Log.d("Log In", "Log in successful")
-                             //snackbar("Đăng nhập thành công")
-                             Toast.makeText(this@SignInActivity, "Đăng nhập thành công", Toast.LENGTH_SHORT).show()
-                         }else{
-                             Log.d("Log In", "Sai tài khoản hoặc mật khẩu")
-                             Toast.makeText(this@SignInActivity, "Sai tài khoản hoặc mật khẩu", Toast.LENGTH_SHORT).show()
-                         }
-                     }
-                  }
+        when (state.stateLogin) {
+            is Success -> {
+                runBlocking {
+                    Timber.tag("Log In: ").d("LogIn...")
+                    launch {
+                        state.stateLogin.invoke()?.let{ listUser ->
+                            if (listUser != null && listUser.isNotEmpty()) {
+                                Log.d("ListData", listUser.toString())
+                                Log.d("Log In", "Log in successful")
+                                //snackbar("Đăng nhập thành công")
+                                Toast.makeText(this@SignInActivity, "Đăng nhập thành công", Toast.LENGTH_SHORT).show()
+                            }else{
+                                Log.d("Log In", "Sai tài khoản hoặc mật khẩu")
+                                Toast.makeText(this@SignInActivity, "Sai tài khoản hoặc mật khẩu", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
 
-              }
-          }
-      }
+                }
+            }
+
+            else -> {}
+        }
     }
 
     override fun getBinding(): ActivitySignInBinding {
