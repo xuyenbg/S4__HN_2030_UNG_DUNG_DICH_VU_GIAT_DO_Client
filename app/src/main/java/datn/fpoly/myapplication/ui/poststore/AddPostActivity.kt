3efcc47bcf3cd1e03d7e40f2ef60ff4bb7a7b502
ITKenTo <monsterlt1105@gmail.com>
2023-10-14 10:21:10 +0700

    dung: post client and store xml

---
+++ app/src/main/java/datn/fpoly/myapplication/ui/poststore/AddPostActivity.kt
@@ -0,0 +1,12 @@
+package datn.fpoly.myapplication.ui.poststore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import datn.fpoly.myapplication.R
+
+class AddPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_post)
+    }
+}
