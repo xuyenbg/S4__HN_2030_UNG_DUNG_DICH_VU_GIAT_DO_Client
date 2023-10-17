ef4a3255c1f750fa2296bd43330dee57e09454e8
anhdat2003 <datktph26567@fpt.edu.vn>
2023-10-17 17:36:21 +0700

    detail store, rate, detail service

---
+++ app/src/main/java/datn/fpoly/myapplication/ui/rate/ListRateActivity.kt
@@ -0,0 +1,12 @@
+package datn.fpoly.myapplication.ui.rate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import datn.fpoly.myapplication.R
+
+class ListRateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_rate)
+    }
+}
---
+++ app/src/main/java/datn/fpoly/myapplication/ui/service/DetailServiceActivity.kt
@@ -0,0 +1,12 @@
+package datn.fpoly.myapplication.ui.service
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import datn.fpoly.myapplication.R
+
+class DetailServiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_service)
+    }
+}
