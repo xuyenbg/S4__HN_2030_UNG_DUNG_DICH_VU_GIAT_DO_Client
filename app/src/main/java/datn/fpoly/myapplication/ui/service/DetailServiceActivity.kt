4b04c92bee56c8270d6d88a5a378dee3f3e2cbb1
Xuyen <xuyen97bgcity1@gmail.com>
2023-11-15 20:13:04 +0700

    xuyen - fix code không thêm vào giỏ hảng do merge code cũ Datpt

--- app/src/main/java/datn/fpoly/myapplication/ui/service/DetailServiceActivity.kt
+++ app/src/main/java/datn/fpoly/myapplication/ui/service/DetailServiceActivity.kt
@@ -25,17 +25,15 @@ import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 import javax.inject.Inject
 
-class DetailServiceActivity : BaseActivity<ActivityDetailServiceBinding>(),
-    DetailServiceViewModel.Factory {
+class DetailServiceActivity : BaseActivity<ActivityDetailServiceBinding>(), DetailServiceViewModel.Factory {
     @Inject
     lateinit var detailServiceFactory: DetailServiceViewModel.Factory
-
     @Inject
     lateinit var authRepo: AuthRepo
     private val viewModel: DetailServiceViewModel by viewModel()
-    private var serviceModel: ServiceModel? = null
+    private var serviceModel : ServiceModel? = null
     private lateinit var adapterService: AdapterService
-    private lateinit var adapterAttribute: AdapterAttribute
+    private lateinit var adapterAttribute : AdapterAttribute
     private lateinit var cart: Order
     private var quality = 1.0
     private var total = 0.0
@@ -43,85 +41,61 @@ class DetailServiceActivity : BaseActivity<ActivityDetailServiceBinding>(),
     override fun onCreate(savedInstanceState: Bundle?) {
         (applicationContext as AppApplication).appComponent.inject(this);
         super.onCreate(savedInstanceState)
-        viewModel.getCart().observe(this) {
-            if (it != null) {
-                cart = it
+        serviceModel = Hawk.get(Common.KEY_SERVICE_DETAIL)
+        views.tvNameService.text = serviceModel?.name
+        views.tvPrice.text = serviceModel?.price?.formatCurrency(unit = serviceModel?.unit) ?: ""
+        views.tvQuantity.text = quality.toInt().toString()
+
+        var nameAttribute=""
+        for (index in 0 until (serviceModel?.attributeList?.size ?: 0)){
+            if(serviceModel?.attributeList?.size!=0){
+                if(index== (serviceModel?.attributeList?.size?.minus(1))){
+                    nameAttribute+= (serviceModel?.attributeList?.get(index)?.name ?: "" )
+                }else{
+                    nameAttribute+= (serviceModel?.attributeList?.get(index)?.name ?: "" ) +">"
+                }
+
             }
         }
-        viewModel.subscribe(this) {
+        views.tvAttribute.text = nameAttribute
+        viewModel.subscribe(this){
             getListService(it)
-            getService(it)
         }
         views.imgBack.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
         adapterService = AdapterService(false)
         views.rcvService.adapter = adapterService
-        adapterService.setListenner(object : AdapterService.ServiceListenner {
-            override fun ServiceOnClick(item: ServiceModel, position: Int) {
-                val intent = Intent(this@DetailServiceActivity, DetailServiceActivity::class.java)
-                intent.putExtra(Common.KEY_ID_SERVICE, item.id)
-                intent.putExtra(Common.KEY_ID_STORE, item.idStore?.id)
-                startActivity(intent)
-                finish()
+        viewModel.getCart().observe(this) {
+            if (it != null) {
+                cart = it
             }
-
-            override fun EditService(serviceModel: ServiceModel) {
-
-            }
-        })
+        }
+        views.tvNameService.text = serviceModel?.name
         adapterAttribute = AdapterAttribute()
         views.rcvItemAttribute.adapter = adapterAttribute
+        serviceModel?.attributeList?.let {
+            adapterAttribute.setData(it)
+        }
         views.btnAddCart.setOnClickListener {
-            for (index in 0 until adapterAttribute.listAttributeSelect.size) {
-                Log.e(
-                    "AAAAAAAAAAA",
-                    "onCreate: name: " + adapterAttribute.listAttributeSelect[index].name
-                )
+            for (index in 0 until adapterAttribute.listAttributeSelect.size){
+                Log.e("AAAAAAAAAAA", "onCreate: name: "+adapterAttribute.listAttributeSelect[index].name )
             }
-            Toast.makeText(
-                this,
-                "list attribute select: size: " + adapterAttribute.listAttributeSelect.size,
-                Toast.LENGTH_SHORT
-            ).show()
-            if (serviceModel != null) {
+            Toast.makeText(this, "list attribute select: size: "+adapterAttribute.listAttributeSelect.size, Toast.LENGTH_SHORT).show()
+            if(serviceModel != null){
                 Log.d("USER", authRepo.getUser().toString())
-                if (cart.idStore != null && cart.idStore?.equals(serviceModel!!.idStore?.id) == false) {
-                    Toast.makeText(
-                        this,
-                        "Bạn có chắc chắn muốn đặt lại không? Nếu bạn tiếp tục, giỏ hàng của bạn sẽ bị xóa.",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                if (cart.idStore != null && cart.idStore?.equals(serviceModel!!.idStore?.id) == false){
+                    Toast.makeText(this, "Bạn có chắc chắn muốn đặt lại không? Nếu bạn tiếp tục, giỏ hàng của bạn sẽ bị xóa.", Toast.LENGTH_SHORT).show()
 
                     cart.idStore = serviceModel?.idStore?.id
 
                     cart.listItem.clear()
-                    cart.listItem.add(
-                        ItemService(
-                            service = serviceModel,
-                            idService = serviceModel?.id,
-                            number = quality,
-                            total = getTotalItem(),
-                            attributeListExtend = adapterAttribute.listAttributeSelect,
-                            attributeList = adapterAttribute.listAttributeSelect.map { attr -> attr.id }
-                                .toMutableList()
-                        )
-                    )
+                    cart.listItem.add(ItemService(service = serviceModel, idService = serviceModel?.id, number = quality, total = getTotalItem(), attributeListExtend = adapterAttribute.listAttributeSelect, attributeList = adapterAttribute.listAttributeSelect.map { attr ->  attr.id}.toMutableList()))
                     viewModel.updateCart(cart)
 
-                } else {
+                }else{
                     cart.idStore = serviceModel!!.idStore?.id
-                    cart.listItem.add(
-                        ItemService(
-                            service = serviceModel,
-                            idService = serviceModel?.id,
-                            number = quality,
-                            total = getTotalItem(),
-                            attributeListExtend = adapterAttribute.listAttributeSelect,
-                            attributeList = adapterAttribute.listAttributeSelect.map { attr -> attr.id }
-                                .toMutableList()
-                        )
-                    )
+                    cart.listItem.add(ItemService(service = serviceModel, idService = serviceModel?.id, number = quality, total = getTotalItem(), attributeListExtend = adapterAttribute.listAttributeSelect, attributeList = adapterAttribute.listAttributeSelect.map { attr ->  attr.id}.toMutableList()))
                     viewModel.updateCart(cart)
                 }
             }
@@ -131,7 +105,7 @@ class DetailServiceActivity : BaseActivity<ActivityDetailServiceBinding>(),
             views.tvQuantity.text = quality.toInt().toString()
         }
         views.btnSubtraction.setOnClickListener {
-            if (quality != 0.0) {
+            if(quality != 0.0){
                 quality -= 1
                 views.tvQuantity.text = quality.toInt().toString()
             }
@@ -139,89 +113,42 @@ class DetailServiceActivity : BaseActivity<ActivityDetailServiceBinding>(),
 
     }
 
-    private fun getTotalItem(): Double {
+    private fun getTotalItem() : Double {
         var priceAttr = 0.0
-        adapterAttribute.listAttributeSelect.forEach { attr -> priceAttr += attr.price }
+        adapterAttribute.listAttributeSelect.forEach{ attr -> priceAttr += attr.price }
         total = (serviceModel!!.price!! + priceAttr) * quality
         return total
     }
 
     override fun onResume() {
         super.onResume()
-        val idStore = intent.getStringExtra(Common.KEY_ID_STORE)
-        val idService = intent.getStringExtra(Common.KEY_ID_SERVICE)
-        idStore?.let {
-            idService.let { it1 ->
-                it1?.let { it2 ->
-                    DetailServiceViewAction.GetListServiceByStore(
-                        it, it2
-                    )
-                }
+        serviceModel?.idStore?.id?.let {
+            serviceModel?.id?.let { it1 ->
+                DetailServiceViewAction.GetListServiceByStore(
+                    it, it1
+                )
             }
         }?.let { viewModel.handle(it) }
-        idService?.let { DetailServiceViewAction.GetServiceById(it) }?.let { viewModel.handle(it) }
     }
-
-    fun getListService(state: DetailServiceViewState) {
-        when (state.stateService) {
-            is Success -> {
+    fun getListService( state: DetailServiceViewState){
+        when(state.stateService){
+            is Success ->{
                 runBlocking {
                     launch {
-                        state.stateService.invoke()?.let {
+                        state.stateService.invoke()?.let{
                             adapterService.setData(it)
-                            Timber.tag("AAAAAAAAA")
-                                .e("getListService: list service size: " + it.size)
+                            Timber.tag("AAAAAAAAA").e("getListService: list service size: "+it.size )
                         }
                     }
                 }
             }
-            is Loading -> {
+            is Loading ->{
                 Timber.tag("AAAAAAAAA").e("getListService: Loading")
             }
             is Fail -> {
                 Timber.tag("AAAAAAAAA").e("getListService: Call Fail")
             }
-            else -> {
-
-            }
-        }
-    }
-
-    private fun getService(state: DetailServiceViewState) {
-        when (state.stateServiceByid) {
-            is Success -> {
-                runBlocking {
-                    launch {
-                        state.stateServiceByid.invoke()?.let {
-                            views.tvNameService.text = it.name
-                            views.tvPrice.text = it.price?.formatCurrency(unit = it.unit) ?: ""
-                            views.tvQuantity.text = quality.toInt().toString()
-                            var nameAttribute = ""
-                            for (index in 0 until (it.attributeList?.size ?: 0)) {
-                                if (it.attributeList?.size != 0) {
-                                    if (index == (it.attributeList?.size?.minus(1))) {
-                                        nameAttribute += (it.attributeList?.get(index)?.name ?: "")
-                                    } else {
-                                        nameAttribute += (it.attributeList?.get(index)?.name
-                                            ?: "") + ">"
-                                    }
-
-                                }
-                            }
-                            views.tvAttribute.text = nameAttribute
-                            it.attributeList?.let { it1 -> adapterAttribute.setData(it1) }
-                            Timber.tag("AAAAAAAAA").e("getListService: list service size: " + it)
-                        }
-                    }
-                }
-            }
-            is Loading -> {
-                Timber.tag("AAAAAAAAA").e("getListService: Loading")
-            }
-            is Fail -> {
-                Timber.tag("AAAAAAAAA").e("getListService: Call Fail")
-            }
-            else -> {
+            else->{
 
             }
         }
