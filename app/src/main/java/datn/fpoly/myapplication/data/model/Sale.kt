eba9fdcf541fc847a58b7fed96affa0a86749348
Xuyen <xuyen97bgcity1@gmail.com>
2023-11-15 22:40:31 +0700

    xuyen - fix nhẹ Sale

--- app/src/main/java/datn/fpoly/myapplication/data/model/Sale.kt
+++ app/src/main/java/datn/fpoly/myapplication/data/model/Sale.kt
@@ -3,7 +3,7 @@ package datn.fpoly.myapplication.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Sale(
-    @SerializedName("id") var id: String? = null,
+    @SerializedName("_id") var id: String? = null,
     @SerializedName("unit") var unit: String? = null,
     @SerializedName("value") var value: Double? = null
 
--- app/src/main/java/datn/fpoly/myapplication/data/model/SaleModel.kt
+++
@@ -1,9 +0,0 @@
-package datn.fpoly.myapplication.data.model
-
-import com.google.gson.annotations.SerializedName
-
-class SaleModel(
-    @SerializedName("_id") var id: String?,
-    @SerializedName("unit") var unit: String?,
-    @SerializedName("value") var value: Int?
-)
